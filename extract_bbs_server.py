78d6db23a47a8e18ec94b23c1b5767909c02a56c
JaviCalvo <javier.calvo@wallapop.com>
2017-01-17 16:27:11 +0100

    working on server for detection

--- extract_bbs_server.py
+++ extract_bbs_server.py
@@ -1,64 +1,114 @@
 import json
-from random import shuffle, random
+
+from flask import Flask, request
+from flask_cors import CORS
 
 import cv2
 
 import mxnet as mx
-import os
 from demo import get_detector
-from detect.detection import Detection, paint_detection, crop_from_detection
-from extract_bbs_config import GPU_ENABLED, GPU_ID, IMAGES_PATH, NUM_IMAGES, EPOCH, NETWORK, DATA_SHAPE, NMS_THRESH, \
-    FORCE_NMS, MEAN_RGB, PREFIX, BATCH_SIZE, EXTENSION, SHOW_TIMER, DETECTION_THRESH, OUTPUT_PATH, JSON_PATH, \
-    CROPS_PATH, \
-    DETECTIONS_PATH
+from detect.detection import Detection, crop_from_detection
+from extract_bbs_config import GPU_ENABLED, GPU_ID, IMAGES_PATH, EPOCH, NETWORK, DATA_SHAPE, NMS_THRESH, \
+    FORCE_NMS, MEAN_RGB, PREFIX, EXTENSION, SHOW_TIMER, DETECTION_THRESH
 from os.path import join
+import random
+import string
+import requests
+import shutil
 
-if __name__ == '__main__':
+
+UPLOAD_FOLDER = '/tmp/'
+
+
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'server responding'
+
+
+@app.route('/detect', methods=['GET', 'POST'])
+def detect():
+    if request.method == 'GET':
+        if 'image_url' in request.args:
+            image_url = request.args.get('image_url')
+            img_path = download_image(image_url)
+            image_path = [img_path]
+        else:
+            return 'error'
+        for j, dets in enumerate(app.dtr.im_detect(image_path, IMAGES_PATH, EXTENSION, SHOW_TIMER)):
+
+            img = cv2.imread(image_path[0])
+            image_name = image_path[j][:-4]
+            img_dets = img.copy()
+
+            detections = []
+            c = 0
+
+            #   Filter detections
+            for det in dets:
+
+                det = Detection(*det)
+                print(det.score)
+                if det.score > DETECTION_THRESH:
+                    detections.append(det)
+
+                    #   Store crop
+                    cv2.imwrite(join(UPLOAD_FOLDER, image_name + '_crop_%04d.jpg' % c), crop_from_detection(det, img))
+                    c += 1
+
+                    #   Paint detection
+                    # paint_detection(det, img_dets, [random() * 255 for ch in range(3)])
+
+            # cv2.imwrite(join(output_path, image_name + '_detections.jpg'), img_dets)
+
+            # detection_info = {
+            #     "image_name": image_name,
+            #     "width": img.shape[1],
+            #     "height": img.shape[0],
+            #     "crops": [det.__dict__ for det in detections]
+            # }
+
+            # with open(join(output_path, image_name + '.json'), 'w') as fp:
+            #     json.dump(detection_info, fp, sort_keys=True, indent=4)
+
+    return ''  # TODO
+
+
+def randomword(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+
+def download_image(url):
+    img_path = UPLOAD_FOLDER + randomword(15) + '.jpg'
+    f = open(img_path, 'w')
+    response = requests.get(url, stream=True)
+    response.raw.decode_content = True
+    shutil.copyfileobj(response.raw, f)
+    f.close()
+    return img_path
+
+
+def start(app):
 
     if GPU_ENABLED:
         ctx = mx.gpu(GPU_ID)
     else:
         ctx = mx.cpu()
 
-    image_path = ['/home/ubuntu/speed-boat/197_0006.jpg']
-    output_path = '/home/ubuntu/speed-boat/out'
-
-    assert len(image_path) > 0, "No valid image specified to detect"
-
     detector = get_detector(NETWORK, PREFIX, EPOCH, DATA_SHAPE, MEAN_RGB, ctx, NMS_THRESH, FORCE_NMS)
 
-    for j, dets in enumerate(detector.im_detect(image_path, IMAGES_PATH, EXTENSION, SHOW_TIMER)):
+    app.dtr = detector
 
-        img = cv2.imread(image_path[0])
-        image_name = image_path[j][:-4]
-        img_dets = img.copy()
+    app.run(threaded=True, host='0.0.0.0', port=5001)
 
-        detections = []
-        c = 0
 
-        #   Filter detections
-        for det in dets:
+if __name__ == '__main__':
 
-            det = Detection(*det)
-            print(det.score)
-            if det.score > DETECTION_THRESH:
-                detections.append(det)
-
-                #   Store crop
-                cv2.imwrite(join(output_path, image_name + '_crop_%04d.jpg' % c), crop_from_detection(det, img))
-                c += 1
-
-                #   Paint detection
-                # paint_detection(det, img_dets, [random() * 255 for ch in range(3)])
-
-        # cv2.imwrite(join(output_path, image_name + '_detections.jpg'), img_dets)
-
-        detection_info = {
-            "image_name": image_name,
-            "width": img.shape[1],
-            "height": img.shape[0],
-            "crops": [det.__dict__ for det in detections]
-        }
-
-        with open(join(output_path, image_name + '.json'), 'w') as fp:
-            json.dump(detection_info, fp, sort_keys=True, indent=4)
+    start(app)
